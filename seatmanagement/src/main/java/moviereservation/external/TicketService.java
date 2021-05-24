82b12dfe01a90bcc7fc7b0e9c6cb23948a0341e7
binaerim <80744169+binaerim@users.noreply.github.com>
2021-05-24 18:18:38 +0900

    Add files via upload

--- seatmanagement/src/main/java/moviereservation/external/TicketService.java
+++ seatmanagement/src/main/java/moviereservation/external/TicketService.java
@@ -1,17 +1,18 @@
 
 package moviereservation.external;
 
+import org.apache.logging.log4j.util.StringBuilderFormattable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Date;
-
-@FeignClient(name="ticketmanagement", url="http://ticketmanagement:8080")
+// modified by jungilkim (PORT 8080->8084)
+@FeignClient(name="ticketmanagement", url="http://ticketmanagement:8084")
 public interface TicketService {
 
-    @RequestMapping(method= RequestMethod.GET, path="/tickets")
-    public void cancelTicket(@RequestBody Ticket ticket);
-
+    @RequestMapping(method= RequestMethod.GET, path="/cancelTicket")
+    // modified by jungilkim
+    public void cancelTicket(@RequestParam("reservationId") Long reservationId, @RequestParam("reservationId") String ticketStatus);
 }
